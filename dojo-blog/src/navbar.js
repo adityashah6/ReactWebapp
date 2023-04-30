94aa374e965e5591068d74b41074736fab494d10
adityashah6 <60235219+adityashah6@users.noreply.github.com>
2023-04-30 16:09:11 -0700

    created web app

---
+++ dojo-blog/src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className = "navbar">
+            <h1>Sew it Seems</h1>
+            <div>
+                <a href= "/">Home</a>
+                <a href= "/create">New Shop</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
