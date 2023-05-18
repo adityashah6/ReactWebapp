3aaac726529d133d9679e34d165fff46fcc499b9
adityashah6 <60235219+adityashah6@users.noreply.github.com>
2023-05-18 14:45:20 -0700

    maded changes

--- my-app/src/components/Checkout.js
+++ my-app/src/components/Checkout.js
@@ -6,7 +6,10 @@ export const Checkout = () => {
     return (
         <div className='Checkout'>
             <h1>Welcome to Checkout. Please checkout your items 💳</h1>
-            <p>Thank your for your puchase</p>
+
+            <div className="thank-you">
+            <h2 className="thank-you-note">Thank you for your purchase</h2>
+            </div>
         </div>
     )
 }
